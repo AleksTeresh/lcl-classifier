28c168ab740161b431aa77053724faa8d8bc2b66
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-01-20 22:38:33 +0200

    Add several problem types

---
+++ src/problem.py
@@ -0,0 +1,41 @@
+from typing import NamedTuple, Set
+
+class BinaryRootedTreeProblem(NamedTuple):
+  allowedConfigs: Set[str]
+
+class TlpProblem(NamedTuple):
+  white_constraint: Set[str]
+  black_constraint: Set[str]
+  white_degree: int
+  black_degree: int
+
+class GenericProblem(NamedTuple):
+  activeConstraints: Set[str]
+  activeAllowAll: bool
+  
+  passiveConstraints: Set[str]
+  passiveAllowAll: bool
+  
+  leafConstraints: Set[str]
+  leafAllowAll: bool
+  
+  rootConstraints: Set[str]
+  rootAllowAll: bool
+  
+  isCycle: bool
+  isPath: bool
+  isDirected: bool
+
+  isTree: bool
+  isRooted: bool
+
+  constrainedDegrees: Set[int]
+
+  isBipartite: bool
+  rootDegree: int
+  degree1: int
+  degree2: int
+  
+
+
+  
