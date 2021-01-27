98f909b62b6eeb840e629f80445c4b5c4ab31072
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-01-27 15:47:13 +0200

    WIP: RE binding

---
+++ src/re_binding.py
@@ -0,0 +1,52 @@
+from problem import GenericProblem
+from parser import parseConfigs
+from config_util import normalizeConstraints
+
+def tryUpperBound(problem):
+  iter_label = [(20,3),(8,4),(9,4)]
+  for pair in iter_label:
+    ub = round_eliminator_ub(problem, pair[0], pair[1])
+    if ub >= 0:
+      problem.set_complexity(Complexity.Constant)
+      problem.constant_upper_bound = min(problem.constant_upper_bound,ub)
+
+def tryLowerBound(problem):
+  iter_label = [(15,5)]
+  for pair in iter_label:
+    lb = round_eliminator_lb(problem, pair[0], pair[1])
+    if lb >= 0:
+      problem.set_complexity(Complexity.Constant)
+      problem.constant_lower_bound = max(problem.constant_lower_bound,lb)
+
+def tryConstantComplexity(problem):
+  tryLowerBound(problem)
+  tryUpperBound(problem)
+
+def classifyByRE(problem: GenericProblem):
+  if problem.isCycle:
+    raise Exception('tlp', 'Cannot classify if the graph is a cycle')
+
+  if problem.isRooted:
+    raise Exception('tlp', 'Cannot classify if the tree is rooted')
+
+  if problem.isDirected:
+    raise Exception('tlp', 'Cannot classify if the path is directed')
+
+  if not problem.isRegular:
+    raise Exception('tlp', 'Cannot classify if the graph is not regular')
+
+  if not problem.rootAllowAll or not problem.leafAllowAll:
+    raise Exception('tlp', 'Leaves and roots must allow all configurations')
+
+  parsedActives = parseConfigs(problem.activeConstraints)
+  parsedPassives = parseConfigs(problem.passiveConstraints)
+
+  activeConstraints = list(normalizeConstraints(parsedActives))
+  passiveConstraints = list(normalizeConstraints(parsedPassives))
+
+  problem = alpha_to_problem(activeConstraints, passiveConstraints)
+  tryConstantComplexity(problem)
+  print(problem)
+  print(problem.constant_lower_bound)
+  print(problem.constant_upper_bound)
+  
