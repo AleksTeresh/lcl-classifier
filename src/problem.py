c18735626a87914a0efd76c17d1f71d688555e51
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-02-09 17:06:27 +0200

    Implement __eq__ for Problem

--- src/problem.py
+++ src/problem.py
@@ -92,3 +92,8 @@ class GenericProblem:
 
     self.__checkParams()
   
+  def __eq__(self, other):
+    if isinstance(other, self.__class__):
+      return self.__dict__ == other.__dict__
+    else:
+      return False
