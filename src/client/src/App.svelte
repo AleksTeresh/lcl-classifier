02ae2d8a3ec3f43530214c438401a6e4a9925d44
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-02-18 17:49:06 +0200

    Add initial Svelte client app

---
+++ src/client/src/App.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	let activeConstraints = ""
+	let passiveConstraints = ""
+
+	let graphType: 'tree' | 'cycle' | 'path' = 'tree'
+	let isDirected: boolean = false
+	let isRooted: boolean = false
+	let isRegular: boolean = false
+</script>
+
+<main>
+	<form>
+		<label>Active configurations:</label>
+		<textarea bind:value={activeConstraints}></textarea>
+
+		<label>Passive configurations:</label>
+		<textarea bind:value={passiveConstraints}></textarea>
+
+		<label>
+			<input type=radio bind:group={graphType} value="tree">
+			Tree
+		</label>
+		<label>
+			<input type=radio bind:group={graphType} value="cycle">
+			Cycle
+		</label>
+		<label>
+			<input type=radio bind:group={graphType} value="path">
+			Path
+		</label>
+
+		<label>
+			<input type=checkbox bind:value={isDirected}>
+			Directed
+		</label>
+		<label>
+			<input type=checkbox bind:value={isRooted}>
+			Rooted
+		</label>
+		<label>
+			<input type=checkbox bind:value={isRegular}>
+			Regular
+		</label>
+
+		{#if graphType === 'path'}
+			<label>Leaf configurations (optional):</label>
+			<textarea bind:value={activeConstraints}></textarea>
+
+			<label>Root configurations (optional):</label>
+			<textarea bind:value={passiveConstraints}></textarea>
+		{/if}
+	</form>
+</main>
