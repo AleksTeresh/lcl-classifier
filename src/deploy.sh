aed3c9ae40f6fca7da46df4511c301d079c8b4a1
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-02-22 12:29:35 +0200

    Deploy script: remove .env file after all docker-containers have been started

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,3 +1,3 @@
 rsync -a --exclude 'client/node_modules' --exclude 'postgres' ./ ubuntu@195.148.21.214:/home/ubuntu/projects/lcl-classifier/newvolume/src
 
-ssh -l ubuntu 195.148.21.214 "cd /home/ubuntu/projects/lcl-classifier/newvolume/src; docker-compose up --build -d"
+ssh -l ubuntu 195.148.21.214 "cd /home/ubuntu/projects/lcl-classifier/newvolume/src; docker-compose up --build -d; rm .env"
