7ec65dd979c1587ad54e1a94ff5060795d6f750f
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-03-07 16:27:47 +0200

    Change backup script: backup to json file as well as to sql

--- src/backup.sh
+++ src/backup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 pg_dumpall -h 195.148.21.214 -U postgres -f ./backup/$(date '+%Y-%m-%d').sql
+
+psql -h 195.148.21.214 -U postgres -c "\copy (SELECT ROW_TO_JSON(t) 
+FROM (SELECT * FROM problems) t)
+TO '$PWD/backup/$(date '+%Y-%m-%d').json';"
