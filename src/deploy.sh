4c32d87d358cd6514e25b36c17ae5314d0e895e9
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-03-26 09:42:34 +0200

    remove old source files as the first deployment step

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,6 +2,8 @@
 
 sh ./backup.sh
 
+ssh -l ubuntu 195.148.21.214 "cd /home/ubuntu/projects/lcl-classifier/newvolume/src; rm -rf ./client; rm -rf ./server"
+
 rsync -a --exclude 'client/node_modules' --exclude 'postgres' --exclude 'backup' --exclude 'data' ./ ubuntu@195.148.21.214:/home/ubuntu/projects/lcl-classifier/newvolume/src
 
 ssh -l ubuntu 195.148.21.214 "cd /home/ubuntu/projects/lcl-classifier/newvolume/src; docker-compose up --build --force-recreate -d; rm .env"
