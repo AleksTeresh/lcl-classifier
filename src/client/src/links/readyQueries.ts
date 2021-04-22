2c8fbe48eff6dbe86422d20b494e3ca49e11c359
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-04-22 19:10:44 +0300

    client: move 2 long example queries to the end of the list
    the idea is that first queries are easy and fast to try for a new
    user

--- src/client/src/links/readyQueries.ts
+++ src/client/src/links/readyQueries.ts
@@ -1,15 +1,5 @@
 export const readyQueries = [
   {
-    href: `${window.location.origin}/?query_graphType=path&query_isDirectedOrRooted=true&query_isRegular=true&query_randLowerBound=%281%29&query_randUpperBound=unsolvable&query_detLowerBound=%281%29&query_detUpperBound=unsolvable&query_activeDegree=2&query_passiveDegree=2&query_labelCount=3&query_activesAllSame=false&query_passivesAllSame=false&query_largestProblemOnly=false&query_smallestProblemOnly=false&query_completelyRandUnclassifiedOnly=false&query_partiallyRandUnclassifiedOnly=false&query_completelyDetUnclassifiedOnly=false&query_partiallyDetUnclassifiedOnly=false&query_excludeIfConfigHasAllOf=&query_excludeIfConfigHasSomeOf=&query_includeIfConfigHasAllOf=&query_includeIfConfigHasSomeOf=&query_fetchStatsOnly=false`,
-    linkText: 'All 3-label problem on directed paths',
-    afterText: '',
-  },
-  {
-    href: `${window.location.origin}/?query_graphType=tree&query_isDirectedOrRooted=false&query_isRegular=true&query_randLowerBound=%281%29&query_randUpperBound=unsolvable&query_detLowerBound=%281%29&query_detUpperBound=unsolvable&query_activeDegree=3&query_passiveDegree=2&query_labelCount=3&query_activesAllSame=false&query_passivesAllSame=false&query_largestProblemOnly=false&query_smallestProblemOnly=false&query_completelyRandUnclassifiedOnly=false&query_partiallyRandUnclassifiedOnly=false&query_completelyDetUnclassifiedOnly=false&query_partiallyDetUnclassifiedOnly=false&query_excludeIfConfigHasAllOf=&query_excludeIfConfigHasSomeOf=&query_includeIfConfigHasAllOf=&query_includeIfConfigHasSomeOf=&query_fetchStatsOnly=false`,
-    linkText: 'All 3-label problems on (3, 2)-biregular unrooted trees',
-    afterText: '',
-  },
-  {
     href: `${window.location.origin}/?query_graphType=tree&query_isDirectedOrRooted=false&query_isRegular=true&query_randLowerBound=%28log*+n%29&query_randUpperBound=%28log*+n%29&query_detLowerBound=%281%29&query_detUpperBound=unsolvable&query_activeDegree=3&query_passiveDegree=2&query_labelCount=3&query_activesAllSame=false&query_passivesAllSame=false&query_largestProblemOnly=false&query_smallestProblemOnly=false&query_completelyRandUnclassifiedOnly=false&query_partiallyRandUnclassifiedOnly=false&query_completelyDetUnclassifiedOnly=false&query_partiallyDetUnclassifiedOnly=false&query_excludeIfConfigHasAllOf=&query_excludeIfConfigHasSomeOf=&query_includeIfConfigHasAllOf=&query_includeIfConfigHasSomeOf=&query_fetchStatsOnly=false`,
     linkText:
       '3-label problems on (3, 2)-biregular unrooted trees with complexity Theta(log* n)',
@@ -25,11 +15,21 @@ export const readyQueries = [
     href: `${window.location.origin}/?query_graphType=tree&query_isDirectedOrRooted=true&query_isRegular=true&query_randLowerBound=%28log*+n%29&query_randUpperBound=%28log*+n%29&query_detLowerBound=%281%29&query_detUpperBound=unsolvable&query_activeDegree=3&query_passiveDegree=2&query_labelCount=4&query_activesAllSame=true&query_passivesAllSame=false&query_largestProblemOnly=false&query_smallestProblemOnly=false&query_completelyRandUnclassifiedOnly=false&query_partiallyRandUnclassifiedOnly=false&query_completelyDetUnclassifiedOnly=false&query_partiallyDetUnclassifiedOnly=false&query_excludeIfConfigHasAllOf=&query_excludeIfConfigHasSomeOf=&query_includeIfConfigHasAllOf=&query_includeIfConfigHasSomeOf=&query_fetchStatsOnly=false`,
     linkText:
       'All Theta(log* n)-solvable among 4-label problems on (3, 2)-biregular rooted trees, where active nodes use monochromatic configs only',
-    afterText: ' - 111 problems in total',
+    afterText: '',
   },
   {
     href: `${window.location.origin}/?query_graphType=tree&query_isDirectedOrRooted=true&query_isRegular=true&query_randLowerBound=%281%29&query_randUpperBound=unsolvable&query_detLowerBound=%281%29&query_detUpperBound=unsolvable&query_activeDegree=3&query_passiveDegree=2&query_labelCount=4&query_activesAllSame=false&query_passivesAllSame=false&query_largestProblemOnly=false&query_smallestProblemOnly=false&query_completelyRandUnclassifiedOnly=false&query_partiallyRandUnclassifiedOnly=true&query_completelyDetUnclassifiedOnly=false&query_partiallyDetUnclassifiedOnly=false&query_excludeIfConfigHasAllOf=&query_excludeIfConfigHasSomeOf=&query_includeIfConfigHasAllOf=&query_includeIfConfigHasSomeOf=&query_fetchStatsOnly=false`,
     linkText: 'Unclassified 4-label problems on (3, 2)-biregular rooted trees',
     afterText: ' - all of them are Omega(n)',
+  },
+  {
+    href: `${window.location.origin}/?query_graphType=tree&query_isDirectedOrRooted=false&query_isRegular=true&query_randLowerBound=%281%29&query_randUpperBound=unsolvable&query_detLowerBound=%281%29&query_detUpperBound=unsolvable&query_activeDegree=3&query_passiveDegree=2&query_labelCount=3&query_activesAllSame=false&query_passivesAllSame=false&query_largestProblemOnly=false&query_smallestProblemOnly=false&query_completelyRandUnclassifiedOnly=false&query_partiallyRandUnclassifiedOnly=false&query_completelyDetUnclassifiedOnly=false&query_partiallyDetUnclassifiedOnly=false&query_excludeIfConfigHasAllOf=&query_excludeIfConfigHasSomeOf=&query_includeIfConfigHasAllOf=&query_includeIfConfigHasSomeOf=&query_fetchStatsOnly=false`,
+    linkText: 'All 3-label problems on (3, 2)-biregular unrooted trees',
+    afterText: '',
+  },
+  {
+    href: `${window.location.origin}/?query_graphType=path&query_isDirectedOrRooted=true&query_isRegular=true&query_randLowerBound=%281%29&query_randUpperBound=unsolvable&query_detLowerBound=%281%29&query_detUpperBound=unsolvable&query_activeDegree=2&query_passiveDegree=2&query_labelCount=3&query_activesAllSame=false&query_passivesAllSame=false&query_largestProblemOnly=false&query_smallestProblemOnly=false&query_completelyRandUnclassifiedOnly=false&query_partiallyRandUnclassifiedOnly=false&query_completelyDetUnclassifiedOnly=false&query_partiallyDetUnclassifiedOnly=false&query_excludeIfConfigHasAllOf=&query_excludeIfConfigHasSomeOf=&query_includeIfConfigHasAllOf=&query_includeIfConfigHasSomeOf=&query_fetchStatsOnly=false`,
+    linkText: 'All 3-label problem on directed paths',
+    afterText: '',
   },
 ]
