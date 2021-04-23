966759588ad54f0ec7a3ec8188bb78dc63588185
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-04-23 15:05:48 +0300

    server: do not expose internal functions of db module

--- src/server/db/__init__.py
+++ src/server/db/__init__.py
@@ -1,27 +1,21 @@
 from .db import update_classifications
 from .db import store_problem_and_classification
-from .db import get_problems
-from .db import get_batch_classifications
 from .db import get_batchless_problem_objs
 from .db import get_classified_problem_obj
 from .db import get_classified_problem_objs
 from .db import store_problem_and_get_id
 from .db import get_problem_count
 from .db import get_batch_classification_by_query
-from .db import get_problem
 from .classified_problem import ClassifiedProblem
 
 __all__ = [
     "update_classifications",
     "store_problem_and_classification",
-    "get_problems",
-    "get_batch_classifications",
     "get_batchless_problem_objs",
     "get_classified_problem_obj",
     "get_classified_problem_objs",
     "store_problem_and_get_id",
     "get_problem_count",
     "get_batch_classification_by_query",
-    "get_problem",
     "ClassifiedProblem",
 ]
