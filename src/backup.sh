0976afa3a254eaeafb514931040e241dfd8c0f7b
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2021-04-27 15:11:28 +0300

    when backing up, create backup folder if it does not exist

--- src/backup.sh
+++ src/backup.sh
@@ -2,6 +2,8 @@
 
 source ./.env
 
+mkdir -p backup
+
 pg_dump "host=195.148.21.214 port=5432 dbname=postgres user=postgres password=$POSTGRES_PASSWORD" -f ./backup/$(date '+%Y-%m-%d').sql
 
 psql "host=195.148.21.214 port=5432 dbname=postgres user=postgres password=$POSTGRES_PASSWORD" -c "\copy (SELECT ROW_TO_JSON(t) 
